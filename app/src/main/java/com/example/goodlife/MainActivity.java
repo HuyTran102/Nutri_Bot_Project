3c9c1deb36e60167c77aad5494e52f9faab03655
macbookair <vuthiquynhchi72@gmail.com>
2024-07-31 10:11:43 +0700

    add
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

---
+++ app/src/main/java/com/example/goodlife/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.goodlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText editTextName, editTextPassword;
+
+    Button signIn;
+
+    TextView signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.name);
+        editTextPassword = findViewById(R.id.password);
+        signIn = findViewById(R.id.sign_in);
+        signUp = findViewById(R.id.sign_up);
+
+//        signUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent  = new Intent(MainActivity.this, RegisterPage.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+    }
+}
