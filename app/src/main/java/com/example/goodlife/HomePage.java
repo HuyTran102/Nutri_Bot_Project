20a55dba85af88e974430596cd783f078da0ad1d
macbookair <vuthiquynhchi72@gmail.com>
2024-08-07 14:53:04 +0700

    add
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/HomePage.java
+++ app/src/main/java/com/example/goodlife/HomePage.java
@@ -2,13 +2,35 @@ package com.example.goodlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button nutritionalStatus, physicalActivity, dietary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        nutritionalStatus.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePage.this, CalculateNutritionalStatus.class);
+            startActivity(intent);
+            finish();
+        });
+
+        physicalActivity.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePage.this, PhysicalActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        dietary.setOnClickListener(view -> {
+            Intent intent = new Intent(HomePage.this, Dietary.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
