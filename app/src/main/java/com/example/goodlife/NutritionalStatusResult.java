cd4eb079b56def09c25bf7dad6af44518b88e0a7
macbookair <vuthiquynhchi72@gmail.com>
2024-08-08 15:21:44 +0700

    add
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/NutritionalStatusResult.java
+++ app/src/main/java/com/example/goodlife/NutritionalStatusResult.java
@@ -2,13 +2,29 @@ package com.example.goodlife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NutritionalStatusResult extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutritional_status_result);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NutritionalStatusResult.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
