9d8e6b94ae12e1e4bf9a270a0122fc3f7947c042
macbookair <vuthiquynhchi72@gmail.com>
2024-08-09 19:40:39 +0700

    Revert "Fix some bugs"
    This reverts commit e192c1f8502165fdaa678cd305d7ea8eef2a3e51.

--- app/src/main/java/com/example/goodlife/MainActivity.java
+++ app/src/main/java/com/example/goodlife/MainActivity.java
@@ -1,47 +1,125 @@
 package com.example.goodlife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity
-{
+import java.util.Calendar;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
 
     TextInputEditText editTextName, editTextPassword;
 
     Button signIn;
 
-    TextView signUp;
+    LinearLayout signUp;
+
+    String signUpDate;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String value = extras.getString("Date");
-            //The key argument here must match that used in the other activity
-        }
+
         editTextName = findViewById(R.id.acc_name);
         editTextPassword = findViewById(R.id.acc_password);
         signIn = findViewById(R.id.sign_in);
         signUp = findViewById(R.id.sign_up);
 
-        signUp.setOnClickListener(new View.OnClickListener()
-        {
+        signUp.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RegisterPage.class);
+            startActivity(intent);
+            finish();
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Intent intent = new Intent(MainActivity.this, RegisterPage.class);
-                startActivity(intent);
-                finish();
+            public void onClick(View view) {
+                if(validDataUsername() || !validDataUserPassword()) {
+                    checkUserData();
+                }
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            signUpDate = extras.getString("Date");
+        }
+    }
+
+    public Boolean validDataUsername() {
+        String name;
+        name = String.valueOf(editTextName.getText());
+        if(name.isEmpty()) {
+            editTextName.setError("Vui lòng nhập vào tên người dùng!");
+            return false;
+        } else {
+            editTextName.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validDataUserPassword() {
+        String password;
+        password = String.valueOf(editTextPassword.getText());
+        if(password.isEmpty()) {
+            editTextName.setError("Vui lòng nhập vào mật khẩu người dùng!");
+            return false;
+        } else {
+            editTextName.setError(null);
+            return true;
+        }
+    }
+    public void checkUserData() {
+        String name, password;
+        name = String.valueOf(editTextName.getText()).trim();
+        password = String.valueOf(editTextPassword.getText()).trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user");
+        Query checkUserDatabase = reference.orderByChild("name").equalTo(name);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.exists()) {
+                    editTextName.setError(null);
+                    String passwordFromDB = snapshot.child(name).child("password").getValue(String.class);
+
+                    if(Objects.equals(passwordFromDB, password)) {
+                        editTextName.setError(null);
+                        Intent intent = new Intent(MainActivity.this, HomePage.class);
+                        intent.putExtra("Date", signUpDate);
+                        intent.putExtra("Name", String.valueOf(editTextName.getText()));
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        editTextPassword.setError("Mật khẩu không đúng!");
+                        editTextPassword.requestFocus();
+                    }
+                } else {
+                    editTextName.setError("Tài khoản không tồn tại!");
+                    editTextName.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
