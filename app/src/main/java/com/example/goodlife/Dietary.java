5a06b12879674c0820a7d439ddd9b7cd111fc1f1
macbookair <vuthiquynhchi72@gmail.com>
2024-08-19 15:04:32 +0700

    add
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/Dietary.java
+++ app/src/main/java/com/example/goodlife/Dietary.java
@@ -1,15 +1,23 @@
 package com.example.goodlife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class Dietary extends AppCompatActivity {
 
     Button backButton;
+    
+    TabLayout tabLayout;
+
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class Dietary extends AppCompatActivity {
         setContentView(R.layout.activity_dietary);
 
         backButton = findViewById(R.id.back_button);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.pageViewer);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new FragmentFood(), "Món ăn");
+        vpAdapter.addFragment(new FragmentGroceries(), "Thực phẩm");
+        vpAdapter.addFragment(new FragmentDrinks(), "Sữa / Đồ uống");
+        vpAdapter.addFragment(new FragmentDiary(), "Nhật ký");
+
+        viewPager.setAdapter(vpAdapter);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
