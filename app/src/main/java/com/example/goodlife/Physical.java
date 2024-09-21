46462c9577e674b5f9f09c91f31e2c8c2f6d07af
macbookair <vuthiquynhchi72@gmail.com>
2024-09-21 20:05:52 +0700

    add
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/Physical.java
+++ app/src/main/java/com/example/goodlife/Physical.java
@@ -3,17 +3,20 @@ package com.example.goodlife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.app.TimePickerDialog;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import java.util.Calendar;
 
 
 public class Physical extends AppCompatActivity {
+    private TimePickerDialog timePickerDialog;
     private Button pickTimeButton;
     private Button backButton;
     @Override
@@ -23,26 +26,10 @@ public class Physical extends AppCompatActivity {
 
         backButton = findViewById(R.id.back_button);
         pickTimeButton = findViewById(R.id.pickTimeButton);
+        pickTimeButton.setText(getTodaysTime());
 
-        pickTimeButton.setOnClickListener(v -> {
-            // Get the current time
-            Calendar calendar = Calendar.getInstance();
-            int hour = calendar.get(Calendar.HOUR_OF_DAY);
-            int minute = calendar.get(Calendar.MINUTE);
-
-            // Create a TimePickerDialog with Holo theme
-            TimePickerDialog timePickerDialog = new TimePickerDialog(
-                    Physical.this,
-                    android.R.style.Theme_Holo_Light_Dialog,  // Use Holo theme here
-                    new TimePickerDialog.OnTimeSetListener() {
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                            // Display the selected time
-                            pickTimeButton.setText(String.format("%02d:%02d", hourOfDay, minute));
-                        }
-                    }, hour, minute, true); // true for 24-hour format
-            timePickerDialog.show();
-        });
+        // use to open the dialog to select the user practice time
+        initTimePicker();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,5 +39,38 @@ public class Physical extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    // Get today time information: hour - minute
+    private String getTodaysTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        return hour + ":" + minute;
+    }
+
+    // Time Picker for user to select their practice time
+    private void initTimePicker() {
+        // Get the current time
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        // Create a TimePickerDialog with Holo theme
+        timePickerDialog = new TimePickerDialog(
+                Physical.this,
+                android.R.style.Theme_Holo_Light_Dialog,  // Use Holo theme here
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // Display the selected time
+                        pickTimeButton.setText(String.format("%02d:%02d", hourOfDay, minute));
+                    }
+                }, hour, minute, true); // true for 24-hour format
+    }
+
+    // open Time picker
+    public void openTimePicker(View view) {
+        timePickerDialog.show();
     }
 }
