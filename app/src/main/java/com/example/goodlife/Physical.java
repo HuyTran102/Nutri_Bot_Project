047694941b620bd1b65f6524528f180afd1f82ec
macbookair <vuthiquynhchi72@gmail.com>
2024-10-09 22:58:22 +0700

    Adding dialog to select activtity level anh each activity of each level in Physical.java
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/Physical.java
+++ app/src/main/java/com/example/goodlife/Physical.java
@@ -3,7 +3,6 @@ package com.example.goodlife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,25 +10,24 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.app.TimePickerDialog;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Physical extends AppCompatActivity {
     private TimePickerDialog timePickerDialog;
     private Button pickTimeButton;
     private Button backButton;
-    private TextView activityLevel;
+    private TextView activityLevel, activitiesOfLevel;
     private Dialog dialog;
-    private ArrayList<String> items = new ArrayList<>();
+    private ArrayList<String> items = new ArrayList<>(), activities = new ArrayList<>();
+    private String level;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +36,7 @@ public class Physical extends AppCompatActivity {
         backButton = findViewById(R.id.back_button);
         pickTimeButton = findViewById(R.id.pick_time_button);
         activityLevel = findViewById(R.id.activity_level);
+        activitiesOfLevel = findViewById(R.id.activities_of_level);
 
         items.add("Nhẹ");
         items.add("Vừa");
@@ -51,8 +50,90 @@ public class Physical extends AppCompatActivity {
             }
         });
 
+        if(level == null) {
+            activities.add("null");
+        }else if(level.equals("Nhẹ")) {
+            activities.add("Chèo thuyền thư giãn");
+            activities.add("Câu cá đứng");
+            activities.add("Làm việc nhà");
+            activities.add("Chơi piano");
+            activities.add("Ngồi yên");
+            activities.add("Yoga");
+            activities.add("Tập thể hình nhẹ");
+            activities.add("Bơi biển, nhẹ");
+            activities.add("Đi bộ vận tốc 3 km/giờ");
+        } else if(level.equals("Vừa")) {
+            activities.add("Bơi biến, vừa");
+            activities.add("Bơi ở bể, 2 km/h");
+            activities.add("Bóng bàn ");
+            activities.add("Aerobic tốc độ chậm");
+            activities.add("Cầu lông"); activities.add("Bắn cung");
+            activities.add("Tập thể hình, vừa");
+            activities.add("Bóng rổ");
+            activities.add("Đạp xe thư giãn");
+            activities.add("Bowling");
+            activities.add("Thể dụng dụng cụ (nhẹ và vừa)");
+            activities.add("Khiêu vũ aerobic hoặc bale");
+            activities.add("Khiêu vũ hiện đại nhanh");
+            activities.add("Câu cá đi bộ và đứng");
+            activities.add("Làm vườn");
+            activities.add("Thể dục dụng cụ");
+            activities.add("Đi bộ đường dài ");
+            activities.add("Nhảy trên bạt lò xo");
+            activities.add("Đi bộ");
+            activities.add("Trượt ván");
+            activities.add("Lướt sóng");
+            activities.add("Bơi lội tốc độ vừa phải");
+            activities.add("Bóng chuyền");
+            activities.add("Đi bộ 6km/giờ");
+            activities.add("Trượt nước");
+        } else if(level.equals("Nặng")) {
+            activities.add("Aerobic tốc độ vừa");
+            activities.add("Tập thể hình nặng");
+            activities.add("Khiêu vũ tốc độ mạnh");
+            activities.add("Đạp xe 20km/giờ");
+            activities.add("Đạp xe trên 30km/giờ");
+            activities.add("Thể dục dụng cụ,mức nặng");
+            activities.add("Khuân vác");
+            activities.add("Bóng đá có thi đấu");
+            activities.add("Chạy bộ 20km/giờ");
+            activities.add("Karate/tae Kwan do");
+            activities.add("Leo núi");
+            activities.add("Trượt patin");
+            activities.add("Trượt patin nhanh");
+            activities.add("Nhảy dây chậm");
+            activities.add("Nhảy dây nhanh");
+            activities.add("Chạy bộ 10 km/giờ");
+            activities.add("Chạy bộ 16km/giờ");
+            activities.add("Chạy bộ 13 km/giờ");
+            activities.add("Chạy bộ 14 km/giờ");
+            activities.add("Chạy bộ 12 km/giờ");
+            activities.add("Chạy bộ 11 km/giờ");
+            activities.add("Đá bóng thông thường");
+            activities.add("Bơi nhanh");
+            activities.add("Bơi vừa");
+            activities.add("Bơi giải trí");
+            activities.add("Bóng chuyền thi đấu/bãi biển");
+            activities.add("Đi bộ 9 km/giờ");
+            activities.add("Đi bộ cầu thang");
+            activities.add("Chạy nước rút");
+            activities.add("Bơi biến nặng");
+            activities.add("Bơi ở bể 2.5 km/h");
+            activities.add("Bơi ở bể 3 km/h");
+            activities.add("Bơi ở bể 3.5 km/h");
+            activities.add("Bơi ở bể 4 km/h");
+        }
+
+        // Set OnClickListener to show the dialog on clicking the TextView
+        activitiesOfLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showActivitiesSearchableSpinnerDialog();
+            }
+        });
+
         // Set current time for both time picker button
-        pickTimeButton.setText(" Thời gian luyện tập: 0 giờ : 0 phút");
+        pickTimeButton.setText(" Thời gian luyện tập 0 giờ : 0 phút");
 
         // use to open the dialog to select the user practice time
         initTimePicker();
@@ -98,14 +179,13 @@ public class Physical extends AppCompatActivity {
         // Create the dialog
         dialog = new Dialog(Physical.this);
         dialog.setContentView(R.layout.dialog_searchable_spinner);
-        dialog.getWindow().setLayout(300, 500);  // Set dialog size as per your requirement
 
         // Initialize dialog views
         EditText editTextSearch = dialog.findViewById(R.id.editTextSearch);
         ListView listView = dialog.findViewById(R.id.listView);
 
         // Set up adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
 
         // Set up search functionality
@@ -126,7 +206,46 @@ public class Physical extends AppCompatActivity {
 
         // Set item click listener
         listView.setOnItemClickListener((adapterView, view, i, l) -> {
-            activityLevel.setText(adapter.getItem(i));
+            level = adapter.getItem(i);
+            activityLevel.setText("Mức độ hoạt động " + adapter.getItem(i));
+            dialog.dismiss();
+        });
+
+        dialog.show();
+    }
+
+    private void showActivitiesSearchableSpinnerDialog() {
+        // Create the dialog
+        dialog = new Dialog(Physical.this);
+        dialog.setContentView(R.layout.dialog_searchable_spinner);
+
+        // Initialize dialog views
+        EditText editTextSearch = dialog.findViewById(R.id.editTextSearch);
+        ListView listView = dialog.findViewById(R.id.listView);
+
+        // Set up adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, activities);
+        listView.setAdapter(adapter);
+
+        // Set up search functionality
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // Set item click listener
+        listView.setOnItemClickListener((adapterView, view, i, l) -> {
+            activitiesOfLevel.setText("Hoạt động " + adapter.getItem(i));
             dialog.dismiss();
         });
 
