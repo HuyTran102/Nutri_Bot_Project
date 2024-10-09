241d01ca4ffdd960e9ff8853e2e2f29007ec4773
macbookair <vuthiquynhchi72@gmail.com>
2024-10-09 23:30:21 +0700

    Fxxing error when adding dialog to select activtity level anh each activity of each level in Physical.java
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/Physical.java
+++ app/src/main/java/com/example/goodlife/Physical.java
@@ -49,80 +49,79 @@ public class Physical extends AppCompatActivity {
                 showSearchableSpinnerDialog();
             }
         });
-
-        if(level == null) {
-            activities.add("null");
-        }else if(level.equals("Nhẹ")) {
-            activities.add("Chèo thuyền thư giãn");
-            activities.add("Câu cá đứng");
-            activities.add("Làm việc nhà");
-            activities.add("Chơi piano");
-            activities.add("Ngồi yên");
-            activities.add("Yoga");
-            activities.add("Tập thể hình nhẹ");
-            activities.add("Bơi biển, nhẹ");
-            activities.add("Đi bộ vận tốc 3 km/giờ");
-        } else if(level.equals("Vừa")) {
-            activities.add("Bơi biến, vừa");
-            activities.add("Bơi ở bể, 2 km/h");
-            activities.add("Bóng bàn ");
-            activities.add("Aerobic tốc độ chậm");
-            activities.add("Cầu lông"); activities.add("Bắn cung");
-            activities.add("Tập thể hình, vừa");
-            activities.add("Bóng rổ");
-            activities.add("Đạp xe thư giãn");
-            activities.add("Bowling");
-            activities.add("Thể dụng dụng cụ (nhẹ và vừa)");
-            activities.add("Khiêu vũ aerobic hoặc bale");
-            activities.add("Khiêu vũ hiện đại nhanh");
-            activities.add("Câu cá đi bộ và đứng");
-            activities.add("Làm vườn");
-            activities.add("Thể dục dụng cụ");
-            activities.add("Đi bộ đường dài ");
-            activities.add("Nhảy trên bạt lò xo");
-            activities.add("Đi bộ");
-            activities.add("Trượt ván");
-            activities.add("Lướt sóng");
-            activities.add("Bơi lội tốc độ vừa phải");
-            activities.add("Bóng chuyền");
-            activities.add("Đi bộ 6km/giờ");
-            activities.add("Trượt nước");
-        } else if(level.equals("Nặng")) {
-            activities.add("Aerobic tốc độ vừa");
-            activities.add("Tập thể hình nặng");
-            activities.add("Khiêu vũ tốc độ mạnh");
-            activities.add("Đạp xe 20km/giờ");
-            activities.add("Đạp xe trên 30km/giờ");
-            activities.add("Thể dục dụng cụ,mức nặng");
-            activities.add("Khuân vác");
-            activities.add("Bóng đá có thi đấu");
-            activities.add("Chạy bộ 20km/giờ");
-            activities.add("Karate/tae Kwan do");
-            activities.add("Leo núi");
-            activities.add("Trượt patin");
-            activities.add("Trượt patin nhanh");
-            activities.add("Nhảy dây chậm");
-            activities.add("Nhảy dây nhanh");
-            activities.add("Chạy bộ 10 km/giờ");
-            activities.add("Chạy bộ 16km/giờ");
-            activities.add("Chạy bộ 13 km/giờ");
-            activities.add("Chạy bộ 14 km/giờ");
-            activities.add("Chạy bộ 12 km/giờ");
-            activities.add("Chạy bộ 11 km/giờ");
-            activities.add("Đá bóng thông thường");
-            activities.add("Bơi nhanh");
-            activities.add("Bơi vừa");
-            activities.add("Bơi giải trí");
-            activities.add("Bóng chuyền thi đấu/bãi biển");
-            activities.add("Đi bộ 9 km/giờ");
-            activities.add("Đi bộ cầu thang");
-            activities.add("Chạy nước rút");
-            activities.add("Bơi biến nặng");
-            activities.add("Bơi ở bể 2.5 km/h");
-            activities.add("Bơi ở bể 3 km/h");
-            activities.add("Bơi ở bể 3.5 km/h");
-            activities.add("Bơi ở bể 4 km/h");
-        }
+//
+////        if(level == null) {
+////            activities.add("null");
+////        }else if(level.equals("Nhẹ")) {
+//            activities.add("Câu cá đứng");
+//            activities.add("Làm việc nhà");
+//            activities.add("Chơi piano");
+//            activities.add("Ngồi yên");
+//            activities.add("Yoga");
+//            activities.add("Tập thể hình nhẹ");
+//            activities.add("Bơi biển, nhẹ");
+//            activities.add("Đi bộ vận tốc 3 km/giờ");
+////        } else if(level.equals("Vừa")) {
+//            activities.add("Bơi biến, vừa");
+//            activities.add("Bơi ở bể, 2 km/h");
+//            activities.add("Bóng bàn ");
+//            activities.add("Aerobic tốc độ chậm");
+//            activities.add("Cầu lông"); activities.add("Bắn cung");
+//            activities.add("Tập thể hình, vừa");
+//            activities.add("Bóng rổ");
+//            activities.add("Đạp xe thư giãn");
+//            activities.add("Bowling");
+//            activities.add("Thể dụng dụng cụ (nhẹ và vừa)");
+//            activities.add("Khiêu vũ aerobic hoặc bale");
+//            activities.add("Khiêu vũ hiện đại nhanh");
+//            activities.add("Câu cá đi bộ và đứng");
+//            activities.add("Làm vườn");
+//            activities.add("Thể dục dụng cụ");
+//            activities.add("Đi bộ đường dài ");
+//            activities.add("Nhảy trên bạt lò xo");
+//            activities.add("Đi bộ");
+//            activities.add("Trượt ván");
+//            activities.add("Lướt sóng");
+//            activities.add("Bơi lội tốc độ vừa phải");
+//            activities.add("Bóng chuyền");
+//            activities.add("Đi bộ 6km/giờ");
+//            activities.add("Trượt nước");
+////        } else if(level.equals("Nặng")) {
+//            activities.add("Aerobic tốc độ vừa");
+//            activities.add("Tập thể hình nặng");
+//            activities.add("Khiêu vũ tốc độ mạnh");
+//            activities.add("Đạp xe 20km/giờ");
+//            activities.add("Đạp xe trên 30km/giờ");
+//            activities.add("Thể dục dụng cụ,mức nặng");
+//            activities.add("Khuân vác");
+//            activities.add("Bóng đá có thi đấu");
+//            activities.add("Chạy bộ 20km/giờ");
+//            activities.add("Karate/tae Kwan do");
+//            activities.add("Leo núi");
+//            activities.add("Trượt patin");
+//            activities.add("Trượt patin nhanh");
+//            activities.add("Nhảy dây chậm");
+//            activities.add("Nhảy dây nhanh");
+//            activities.add("Chạy bộ 10 km/giờ");
+//            activities.add("Chạy bộ 16km/giờ");
+//            activities.add("Chạy bộ 13 km/giờ");
+//            activities.add("Chạy bộ 14 km/giờ");
+//            activities.add("Chạy bộ 12 km/giờ");
+//            activities.add("Chạy bộ 11 km/giờ");
+//            activities.add("Đá bóng thông thường");
+//            activities.add("Bơi nhanh");
+//            activities.add("Bơi vừa");
+//            activities.add("Bơi giải trí");
+//            activities.add("Bóng chuyền thi đấu/bãi biển");
+//            activities.add("Đi bộ 9 km/giờ");
+//            activities.add("Đi bộ cầu thang");
+//            activities.add("Chạy nước rút");
+//            activities.add("Bơi biến nặng");
+//            activities.add("Bơi ở bể 2.5 km/h");
+//            activities.add("Bơi ở bể 3 km/h");
+//            activities.add("Bơi ở bể 3.5 km/h");
+//            activities.add("Bơi ở bể 4 km/h");
+////        }
 
         // Set OnClickListener to show the dialog on clicking the TextView
         activitiesOfLevel.setOnClickListener(new View.OnClickListener() {
@@ -209,6 +208,85 @@ public class Physical extends AppCompatActivity {
             level = adapter.getItem(i);
             activityLevel.setText("Mức độ hoạt động " + adapter.getItem(i));
             dialog.dismiss();
+
+            if(adapter.getItem(i) == null) {
+                activities.add("null");
+            }else if(level.equals("Nhẹ")) {
+                activities.clear();
+
+                activities.add("Câu cá đứng");
+                activities.add("Làm việc nhà");
+                activities.add("Chơi piano");
+                activities.add("Ngồi yên");
+                activities.add("Yoga");
+                activities.add("Tập thể hình nhẹ");
+                activities.add("Bơi biển, nhẹ");
+                activities.add("Đi bộ vận tốc 3 km/giờ");
+            } else if(adapter.getItem(i).equals("Vừa")) {
+                activities.clear();
+
+                activities.add("Bơi biến, vừa");
+                activities.add("Bơi ở bể, 2 km/h");
+                activities.add("Bóng bàn ");
+                activities.add("Aerobic tốc độ chậm");
+                activities.add("Cầu lông"); activities.add("Bắn cung");
+                activities.add("Tập thể hình, vừa");
+                activities.add("Bóng rổ");
+                activities.add("Đạp xe thư giãn");
+                activities.add("Bowling");
+                activities.add("Thể dụng dụng cụ (nhẹ và vừa)");
+                activities.add("Khiêu vũ aerobic hoặc bale");
+                activities.add("Khiêu vũ hiện đại nhanh");
+                activities.add("Câu cá đi bộ và đứng");
+                activities.add("Làm vườn");
+                activities.add("Thể dục dụng cụ");
+                activities.add("Đi bộ đường dài ");
+                activities.add("Nhảy trên bạt lò xo");
+                activities.add("Đi bộ");
+                activities.add("Trượt ván");
+                activities.add("Lướt sóng");
+                activities.add("Bơi lội tốc độ vừa phải");
+                activities.add("Bóng chuyền");
+                activities.add("Đi bộ 6km/giờ");
+                activities.add("Trượt nước");
+            } else if(adapter.getItem(i).equals("Nặng")) {
+                activities.clear();
+
+                activities.add("Aerobic tốc độ vừa");
+                activities.add("Tập thể hình nặng");
+                activities.add("Khiêu vũ tốc độ mạnh");
+                activities.add("Đạp xe 20km/giờ");
+                activities.add("Đạp xe trên 30km/giờ");
+                activities.add("Thể dục dụng cụ,mức nặng");
+                activities.add("Khuân vác");
+                activities.add("Bóng đá có thi đấu");
+                activities.add("Chạy bộ 20km/giờ");
+                activities.add("Karate/tae Kwan do");
+                activities.add("Leo núi");
+                activities.add("Trượt patin");
+                activities.add("Trượt patin nhanh");
+                activities.add("Nhảy dây chậm");
+                activities.add("Nhảy dây nhanh");
+                activities.add("Chạy bộ 10 km/giờ");
+                activities.add("Chạy bộ 16km/giờ");
+                activities.add("Chạy bộ 13 km/giờ");
+                activities.add("Chạy bộ 14 km/giờ");
+                activities.add("Chạy bộ 12 km/giờ");
+                activities.add("Chạy bộ 11 km/giờ");
+                activities.add("Đá bóng thông thường");
+                activities.add("Bơi nhanh");
+                activities.add("Bơi vừa");
+                activities.add("Bơi giải trí");
+                activities.add("Bóng chuyền thi đấu/bãi biển");
+                activities.add("Đi bộ 9 km/giờ");
+                activities.add("Đi bộ cầu thang");
+                activities.add("Chạy nước rút");
+                activities.add("Bơi biến nặng");
+                activities.add("Bơi ở bể 2.5 km/h");
+                activities.add("Bơi ở bể 3 km/h");
+                activities.add("Bơi ở bể 3.5 km/h");
+                activities.add("Bơi ở bể 4 km/h");
+            }
         });
 
         dialog.show();
