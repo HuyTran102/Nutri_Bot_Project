6f3cdef1fc37af17c4bd75d272b391af5e9a4e64
macbookair <vuthiquynhchi72@gmail.com>
2024-11-27 21:03:11 +0700

    Adding contact with nutritionist
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

---
+++ app/src/main/java/com/example/goodlife/ContactWithNutritionist.java
@@ -0,0 +1,29 @@
+package com.example.goodlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ContactWithNutritionist extends AppCompatActivity {
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_with_nutritionist);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactWithNutritionist.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
