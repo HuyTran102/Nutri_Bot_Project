588785d59e40c493193c971fa089362148a8c0c1
macbookair <vuthiquynhchi72@gmail.com>
2024-12-01 15:58:48 +0700

    building recommend menu and update some bug
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

---
+++ app/src/main/java/com/example/goodlife/RecommendMenuNum1.java
@@ -0,0 +1,29 @@
+package com.example.goodlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RecommendMenuNum1 extends AppCompatActivity {
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend_menu_num1);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecommendMenuNum1.this, HomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/goodlife/RecommendMenuNum2.java
@@ -0,0 +1,14 @@
+package com.example.goodlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecommendMenuNum2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend_menu_num2);
+    }
+}
