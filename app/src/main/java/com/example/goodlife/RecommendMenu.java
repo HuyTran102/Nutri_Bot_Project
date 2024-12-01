ac401ea1ccc04ba9bf636416835c18f31fffc02f
macbookair <vuthiquynhchi72@gmail.com>
2024-12-01 15:58:48 +0700

    adding recommend menu template
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/RecommendMenu.java
+++ app/src/main/java/com/example/goodlife/RecommendMenu.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RecommendMenu extends AppCompatActivity {
+    private TextView menu1, menu2, menu3;
     private Button backButton;
 
     @Override
@@ -15,7 +17,37 @@ public class RecommendMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_menu);
 
+        menu1 = findViewById(R.id.menu_1);
+        menu2 = findViewById(R.id.menu_2);
+        menu3 = findViewById(R.id.menu_3);
         backButton = findViewById(R.id.back_button);
+
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecommendMenu.this, RecommendMenuNum1.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecommendMenu.this, RecommendMenuNum2.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        menu3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecommendMenu.this, RecommendMenuNum3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/goodlife/RecommendMenuNum2.java
@@ -0,0 +1,29 @@
+package com.example.goodlife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RecommendMenuNum2 extends AppCompatActivity {
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend_menu_num2);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecommendMenuNum2.this, RecommendMenu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
