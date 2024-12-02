1cc19a7666b8471a7397852df2a3fa8eed6b6e6d
macbookair <vuthiquynhchi72@gmail.com>
2024-12-02 23:25:40 +0700

    update recommend menu view
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/RecommendMenu.java
+++ app/src/main/java/com/example/goodlife/RecommendMenu.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class RecommendMenu extends AppCompatActivity {
-    private TextView menu1, menu2, menu3;
+    private LinearLayout menu1, menu2, menu3;
     private Button backButton;
 
     @Override
