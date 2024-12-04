bdcf044c57552cfd1d32111f6b87d6fe310778b8
macbookair <vuthiquynhchi72@gmail.com>
2024-12-04 11:01:41 +0700

    Add tracking diagram to project
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/TrackingDiagram.java
+++ app/src/main/java/com/example/goodlife/TrackingDiagram.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-//import com.github.mikephil.charting.charts.LineChart;
-//import com.github.mikephil.charting.components.XAxis;
-//import com.github.mikephil.charting.components.YAxis;
-//import com.github.mikephil.charting.data.Entry;
-//import com.github.mikephil.charting.data.LineData;
-//import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 public class TrackingDiagram extends AppCompatActivity {
     private Button backButton;
@@ -23,6 +25,33 @@ public class TrackingDiagram extends AppCompatActivity {
         setContentView(R.layout.activity_tracking_diagram);
 
         backButton = findViewById(R.id.back_button);
+
+        LineChart lineChart = findViewById(R.id.lineChart);
+
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 2));
+        entries.add(new Entry(1, 4));
+        entries.add(new Entry(2, 6));
+        entries.add(new Entry(3, 3));
+        entries.add(new Entry(4, 10));
+        entries.add(new Entry(5, 2));
+        entries.add(new Entry(6, 1));
+        entries.add(new Entry(7, 6));
+        entries.add(new Entry(8, 5));
+        entries.add(new Entry(9, 13));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Thống kê năng lượng");
+        dataSet.setColor(getResources().getColor(R.color.red_pink));
+        dataSet.setLineWidth(4f);
+        dataSet.setValueTextColor(getResources().getColor(R.color.dark_green));
+        dataSet.setValueTextSize(15f);
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        lineChart.getDescription().setEnabled(false);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        lineChart.invalidate();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
