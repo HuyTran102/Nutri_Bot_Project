b64efe42eaca8c1eadc16a253f9e2bad3370584f
macbookair <vuthiquynhchi72@gmail.com>
2024-12-04 22:38:44 +0700

    Loading sum kcal from database
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/TrackingDiagram.java
+++ app/src/main/java/com/example/goodlife/TrackingDiagram.java
@@ -1,11 +1,16 @@
 package com.example.goodlife;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -13,10 +18,31 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.IsoFields;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
 
 public class TrackingDiagram extends AppCompatActivity {
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private ArrayList<Entry> entries = new ArrayList<>();
+    private LocalDate currentDate;
+    private LineChart lineChart;
+    private WeekFields weekFields;
+    private LocalDate startOfWeek;
+    private LocalDate endOfWeek;
+    private String name;
     private Button backButton;
 
     @Override
@@ -24,11 +50,13 @@ public class TrackingDiagram extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_diagram);
 
+        lineChart = findViewById(R.id.lineChart);
         backButton = findViewById(R.id.back_button);
 
-        LineChart lineChart = findViewById(R.id.lineChart);
+        SharedPreferences sp = getSharedPreferences("Data", Context.MODE_PRIVATE);
 
-        ArrayList<Entry> entries = new ArrayList<>();
+        name = sp.getString("Name",null);
+
         entries.add(new Entry(0, 2));
         entries.add(new Entry(1, 4));
         entries.add(new Entry(2, 6));
@@ -51,7 +79,17 @@ public class TrackingDiagram extends AppCompatActivity {
         lineChart.getDescription().setEnabled(false);
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
 
-        lineChart.invalidate();
+        currentDate = LocalDate.now();
+
+        weekFields = WeekFields.of(Locale.getDefault());
+
+        startOfWeek = currentDate.with(weekFields.dayOfWeek(), 1);
+
+        endOfWeek = currentDate.with(weekFields.dayOfWeek(), 7);
+
+        showListData();
+
+//        Toast.makeText(this, startOfWeek + " " + endOfWeek, Toast.LENGTH_SHORT).show();
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +99,34 @@ public class TrackingDiagram extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void showListData() {
+        firebaseFirestore.collection("GoodLife")
+                .document(name)
+                .collection("Nhật kí")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            // Loop through all documents
+                            for (int i = startOfWeek.getDayOfMonth(); i <= endOfWeek.getDayOfMonth(); i++) {
+                                int sumKcal = 0;
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    int day = Integer.parseInt(document.getString("day")), month = Integer.parseInt(document.getString("month")), year = Integer.parseInt(document.getString("year"));
+                                    if ((i == day && day == i)
+                                            && (startOfWeek.getMonthValue() == month && endOfWeek.getMonthValue() == month)
+                                            && (startOfWeek.getYear() == month && endOfWeek.getYear() == month)) {
+                                        sumKcal += Integer.parseInt(document.getString("kcal"));
+                                    }
+                                }
+                                if(sumKcal != 0) Toast.makeText(TrackingDiagram.this, "" + sumKcal, Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Log.w("Firestore", "Error getting documents", task.getException());
+                        }
+                    }
+                });
     }
 }
