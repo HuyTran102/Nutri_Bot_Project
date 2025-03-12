1de1d4a98d52e80810b1adea0dcd82d41923863e
macbookair <vuthiquynhchi72@gmail.com>
2025-03-12 22:33:56 +0700

    update notification to look better and more professional
    Signed-off-by: macbookair <vuthiquynhchi72@gmail.com>

--- app/src/main/java/com/example/goodlife/Notification.java
+++ app/src/main/java/com/example/goodlife/Notification.java
@@ -56,6 +56,8 @@ public class Notification extends AppCompatActivity {
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
         recyclerView = findViewById(R.id.recycleView);
         backButton = findViewById(R.id.back_button);
 
